500b8e26259527f743812213f80a35c374ceba7b
Jordy Meijer (student) <s1195262@student.windesheim.nl>
2024-10-30 23:11:54 +0100

    debugging

--- js/script.js
+++ js/script.js
@@ -82,12 +82,3 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     // Clear the form
     document.getElementById('contact-form').reset();
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const headerToggle = document.querySelector('.header-toggle');
-    const headerContent = document.querySelector('.header-content');
-
-    headerToggle.addEventListener('click', function() {
-        headerContent.classList.toggle('active');
-    });
-});
